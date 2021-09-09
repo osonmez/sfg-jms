d5e82fc21a803ff3236bb1fde667c5a06f3fa316
Omer Sonmez <omer.sonmez@nokia.com>
2021-09-09 11:35:37 +0300

    send and reply together

--- src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
+++ src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
@@ -1,8 +1,12 @@
 package guru.springframework.sfgjms.listener;
 
+import java.util.UUID;
+
+import javax.jms.JMSException;
 import javax.jms.Message;
 
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -10,21 +14,39 @@ import org.springframework.stereotype.Component;
 
 import guru.springframework.sfgjms.config.JmsConfig;
 import guru.springframework.sfgjms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Component
 public class HelloMessageListener {
+
+    private final JmsTemplate jmsTemplate;
 
     @JmsListener(destination = JmsConfig.MY_QUEUE)
     public void listen(@Payload HelloWorldMessage helloWorldMessage,
                        @Headers MessageHeaders headers, Message message){
 
-        System.out.println("I Got a Message!!!!!");
+        //System.out.println("I Got a Message!!!!!");
 
-        System.out.println(helloWorldMessage);
+       // System.out.println(helloWorldMessage);
 
 
         // uncomment and view to see retry count in debugger
        // throw new RuntimeException("foo");
+
+    }
+
+    @JmsListener(destination = JmsConfig.MY_SEND_RCV_QUEUE)
+    public void listenForHello(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers, Message message) throws JMSException {
+
+        HelloWorldMessage payloadMsg = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("World!!")
+                .build();
+
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
 
     }
 
